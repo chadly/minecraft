e4291a5210e4be82ce9bd9a7b3d32fd2f2307995
Chad Lee <git@chadly.net>
2023-12-30 07:03:39 -0600

    ✨ feat: streamlined world creation

---
+++ start.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+base_port=19132
+icon_url="https://micdoodle8.com/wp-content/uploads/2023/07/enderman-micdoodle8-1-1024x571.jpg"
+server_address="qube.lan"
+
+# Delete the existing docker-compose-worlds.yml file if it exists
+if [ -f docker-compose-worlds.yml ]; then
+    rm docker-compose-worlds.yml
+fi
+
+# Copy the existing docker-compose.yml to docker-compose-worlds.yml
+cp docker-compose.yml docker-compose-worlds.yml
+
+# Initialize servers.json
+echo "[]" > bedrockconnect/servers.json
+
+# Read each world from the JSON file
+cat minecraft/worlds.json | jq -c '.[]' | while read -r world; do
+  id=$(echo $world | jq -r '.id')
+  motd=$(echo $world | jq -r '.MOTD')
+
+  # Calculate the port for the current service
+  port=$((base_port + 1))
+  base_port=$port
+
+  # Append the service configuration to the docker-compose-worlds.yml file
+  cat << EOF >> docker-compose-worlds.yml
+  $id:
+    container_name: minecraft_$id
+    build:
+      context: ./minecraft
+    depends_on:
+      - bedrockconnect
+      - router
+    tty: true
+    stdin_open: true
+    restart: unless-stopped
+    environment:
+      MOTD: "$motd"
+    ports:
+      - "$port:19132/udp"
+    volumes:
+      - "./minecraft/worlds/$id:/data:Z"
+
+EOF
+
+  # Update servers.json file with the new server entry
+  jq --arg name "$motd" \
+     --arg iconUrl "$icon_url" \
+     --arg address "$server_address" \
+     --argjson port "$port" \
+     '. += [{"name": $name, "iconUrl": $iconUrl, "address": $address, "port": $port}]' \
+     bedrockconnect/servers.json > bedrockconnect/servers.tmp && mv bedrockconnect/servers.tmp bedrockconnect/servers.json
+done
+
+docker-compose -f docker-compose-worlds.yml up
